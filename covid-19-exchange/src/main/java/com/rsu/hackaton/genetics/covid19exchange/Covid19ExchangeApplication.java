add92b5acb082d145f2536fd18cd0b6612eab98f
Kristaps Horns <kristaps@trappswise.nl>
2020-10-23 23:21:27 +0300

    Init.

---
+++ covid-19-exchange/src/main/java/com/rsu/hackaton/genetics/covid19exchange/Covid19ExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.rsu.hackaton.genetics.covid19exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19ExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19ExchangeApplication.class, args);
+	}
+
+}
---
+++ covid-19-exchange/src/test/java/com/rsu/hackaton/genetics/covid19exchange/Covid19ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsu.hackaton.genetics.covid19exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
